3fa426aebef4151ccc57801d4d561f1ff267b72c
Tobias Kristensen <tobiaskristensen12@gmail.com>
2023-06-07 23:46:52 +0200

    docs: Add docstrings for the BaseModel class

--- OmmelSamvirke.Domain/Common/BaseModel.cs
+++ OmmelSamvirke.Domain/Common/BaseModel.cs
@@ -1,8 +1,25 @@
 ﻿namespace OmmelSamvirke.Domain.Common;
 
+/// <summary>
+/// BaseModel contains the basic fields that all models must contain.
+/// Thus, all models must extend from this class.
+/// </summary>
 public abstract class BaseModel
 {
+    /// <summary>
+    /// The integer Id of a model instance.
+    /// It is assumed that any DB used with Ommel Samvirke will
+    /// use integer identifiers for all table types.
+    /// </summary>
     public int Id { get; set; }
+    
+    /// <summary>
+    /// Describes when the instance of the model was first created.
+    /// </summary>
     public DateTime DateCreated { get; set; }
+    
+    /// <summary>
+    /// Describes when the instance of the model was last modified.
+    /// </summary>
     public DateTime DateModified { get; set; }
 }
