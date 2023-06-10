4d31f34d0ffa33f1441886befa933810d793fd73
Tobias Kristensen <tobiaskristensen12@gmail.com>
2023-06-10 22:54:01 +0200

    feat: Add NewsletterComment Models
    Add the model NewsletterComment for adding comments
    to newsletters

---
+++ OmmelSamvirke.Domain.UnitTests/Features/Newsletters/Models/NewsletterCommentTests.cs
@@ -0,0 +1,71 @@
+﻿using OmmelSamvirke.Domain.Features.Newsletters.Models;
+
+namespace OmmelSamvirke.Domain.UnitTests.Features.Newsletters.Models;
+
+public class NewsletterCommentTests
+{
+    private int _defaultModelId;
+    private DateTime _defaultDateCreated;
+    private DateTime _defaultDateModified;
+    private int _defaultUserId;
+    private int _defaultNewsletterId;
+    private string _defaultContent = null!;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _defaultModelId = 1;
+        _defaultDateCreated = DateTime.UtcNow;
+        _defaultDateModified = _defaultDateCreated;
+        _defaultUserId = 2;
+        _defaultNewsletterId = 3;
+        _defaultContent = "This is a test comment";
+    }
+
+    [Test]
+    public void DefaultConstructor_InitializesParameters()
+    {
+        NewsletterComment newsletterComment = new(_defaultUserId, _defaultNewsletterId, _defaultContent);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(newsletterComment.UserId, Is.EqualTo(_defaultUserId));
+            Assert.That(newsletterComment.NewsletterId, Is.EqualTo(_defaultNewsletterId));
+            Assert.That(newsletterComment.Content, Is.EqualTo(_defaultContent));
+        });
+    }
+
+    [Test]
+    public void BaseConstructor_InitializesParameters()
+    {
+        NewsletterComment newsletterComment = new(
+            _defaultModelId,
+            _defaultDateCreated,
+            _defaultDateModified,
+            _defaultUserId,
+            _defaultNewsletterId,
+            _defaultContent
+        );
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(newsletterComment.Id, Is.EqualTo(_defaultModelId));
+            Assert.That(newsletterComment.DateCreated, Is.EqualTo(_defaultDateCreated));
+            Assert.That(newsletterComment.DateModified, Is.EqualTo(_defaultDateModified));
+            Assert.That(newsletterComment.UserId, Is.EqualTo(_defaultUserId));
+            Assert.That(newsletterComment.NewsletterId, Is.EqualTo(_defaultNewsletterId));
+            Assert.That(newsletterComment.Content, Is.EqualTo(_defaultContent));
+        });
+    }
+
+    [Test]
+    public void Content_SetContent_UpdatesContent()
+    {
+        NewsletterComment newsletterComment = new(_defaultUserId, _defaultNewsletterId, _defaultContent);
+        const string updatedComment = "This is an updated comment";
+
+        newsletterComment.Content = updatedComment;
+        
+        Assert.That(newsletterComment.Content, Is.EqualTo(updatedComment));
+    }
+}
--- OmmelSamvirke.Domain/Features/Newsletters/Models/NewsletterComment.cs
+++ OmmelSamvirke.Domain/Features/Newsletters/Models/NewsletterComment.cs
@@ -1,6 +1,82 @@
-﻿namespace OmmelSamvirke.Domain.Features.Newsletters.Models;
+﻿using Ganss.Xss;
+using OmmelSamvirke.Domain.Common;
 
-public class NewsletterComment
+namespace OmmelSamvirke.Domain.Features.Newsletters.Models;
+
+/// <summary>
+/// This class represents a comment that can be added to a <see cref="Newsletter"/>.
+/// Comment are created by users.
+/// </summary>
+public class NewsletterComment : BaseModel
 {
+
+    /// <summary>
+    /// The Id of the user who posted the comment
+    /// </summary>
+    public int UserId { get; private set; }
     
+    /// <summary>
+    /// The Id of the newsletter the comment was posted to
+    /// </summary>
+    public int NewsletterId { get; private set; }
+
+    /// <summary>
+    /// The content of the comment.
+    /// Must be between 2-15.000 characters long.
+    /// </summary>
+    public string Content { get; set; } = null!;
+
+    /// <summary>
+    /// Create an instance of a newsletter comment.
+    /// This constructor should be used when the model has not yet been saved to the database.
+    /// </summary>
+    /// <param name="userId"><see cref="UserId"/></param>
+    /// <param name="newsletterId"><see cref="NewsletterId"/></param>
+    /// <param name="content"><see cref="Content"/></param>
+    public NewsletterComment(int userId, int newsletterId, string content)
+    {
+        Initialize(userId, newsletterId, content);
+    }
+    
+    /// <summary>
+    /// Create an instance of a newsletter comment.
+    /// This constructor should be used when the model is being loaded from the database.
+    /// </summary>
+    /// <param name="id"><see cref="BaseModel.Id"/></param>
+    /// <param name="dateCreated"><see cref="BaseModel.DateCreated"/></param>
+    /// <param name="dateModified"><see cref="BaseModel.DateModified"/></param>
+    /// <param name="userId"><see cref="UserId"/></param>
+    /// <param name="newsletterId"><see cref="NewsletterId"/></param>
+    /// <param name="content"><see cref="Content"/></param>
+    public NewsletterComment(
+        int id,
+        DateTime dateCreated,
+        DateTime dateModified,
+        int userId,
+        int newsletterId,
+        string content
+    ) : base(id, dateCreated, dateModified)
+    {
+        Initialize(userId, newsletterId, content);
+    }
+
+    private void Initialize(int userId, int newsletterId, string content)
+    {
+        HtmlSanitizer htmlSanitizer = new();
+        string sanitizedContent = htmlSanitizer.Sanitize(content);
+
+        ValidateContent(sanitizedContent);
+        
+        UserId = userId;
+        NewsletterId = newsletterId;
+        Content = sanitizedContent;
+    }
+
+    private static void ValidateContent(string content)
+    {
+        if (content.Length is < 2 or > 15_000)
+        {
+            throw new ArgumentException("Property Content must be between 2-15.000 characters long");
+        }
+    }
 }
