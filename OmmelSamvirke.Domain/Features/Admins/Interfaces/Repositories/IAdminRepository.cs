5b90b34987450329ff87b9a4bc6c4686b685dee2
Tobias Kristensen <tobiaskristensen12@gmail.com>
2023-07-10 14:49:28 +0200

    feat: Add Admin model and repository interface
    Add a model for a class representing an Admin user.
    A repository interface which extends IGenericRepository has also been added.

---
+++ OmmelSamvirke.Domain/Features/Admins/Interfaces/Repositories/IAdminRepository.cs
@@ -0,0 +1,9 @@
+﻿using OmmelSamvirke.Domain.Common.Interfaces;
+using OmmelSamvirke.Domain.Features.Admins.Models;
+
+namespace OmmelSamvirke.Domain.Features.Admins.Interfaces.Repositories;
+
+public interface IAdminRepository : IGenericRepository<Admin>
+{
+    
+}
