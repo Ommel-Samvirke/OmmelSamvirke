2f019fc9fd21d8821404e917de94202bad5e99bd
Tobias Kristensen <tobiaskristensen12@gmail.com>
2023-07-10 14:53:58 +0200

    feat: Add TempUpdateAsync to IPageTemplateRepo
    Add the method TempUpdateAsync to the repository interface IPageTemplateRepository.
    The method should be used when updating an entity via optimistic locking, since the
    implementation of the method must save the changes in a temporary entry.

--- OmmelSamvirke.Domain/Features/Pages/Interfaces/Repositories/IPageTemplateRepository.cs
+++ OmmelSamvirke.Domain/Features/Pages/Interfaces/Repositories/IPageTemplateRepository.cs
@@ -5,5 +5,14 @@ namespace OmmelSamvirke.Domain.Features.Pages.Interfaces.Repositories;
 
 public interface IPageTemplateRepository : IGenericRepository<PageTemplate>
 {
-
+    /// <summary>
+    /// Updates a temporary version of a <see cref="PageTemplate"/> with the new state.
+    /// This aids in implementing optimistic concurrency, meaning that the update will only be applied if
+    /// the <see cref="PageTemplate"/> has not been updated since the last time it was fetched and when a non-temporary
+    /// update action is triggered.
+    /// </summary>
+    /// <param name="entity">The new state of the <see cref="PageTemplate"/></param>
+    /// <param name="adminId">The Id of the admin that made the update</param>
+    /// <returns></returns>
+    Task<PageTemplate> TempUpdateAsync(PageTemplate entity, int adminId);
 }
