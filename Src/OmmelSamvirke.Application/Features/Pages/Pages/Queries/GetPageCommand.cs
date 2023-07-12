a925d872be39443eaf9d5e6349322e70c01f72ed
Tobias Kristensen <tobiaskristensen12@gmail.com>
2023-07-12 11:35:26 +0200

    feat: Add feature "GetPageById"
    Add Command, Handler, and Validator for the feature "Get a Page by its id"

---
+++ Src/OmmelSamvirke.Application/Features/Pages/Pages/Queries/GetPageCommand.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using MediatR;
+using OmmelSamvirke.Application.Errors;
+using OmmelSamvirke.Application.Features.Pages.DTOs;
+using OmmelSamvirke.Application.Features.Pages.Pages.Validators;
+using OmmelSamvirke.Domain.Features.Pages.Interfaces.Repositories;
+using OmmelSamvirke.Domain.Features.Pages.Models;
+
+namespace OmmelSamvirke.Application.Features.Pages.Pages.Queries;
+
+public class GetPageCommand : IRequest<PageDto>
+{
+    public int PageId { get; }
+
+    public GetPageCommand(int pageId)
+    {
+        PageId = pageId;
+    }
+}
+
+public class GetPageCommandHandler : IRequestHandler<GetPageCommand, PageDto>
+{
+    private readonly IMapper _mapper;
+    private readonly IPageRepository _pageRepository;
+
+    public GetPageCommandHandler(IMapper mapper, IPageRepository pageRepository)
+    {
+        _mapper = mapper;
+        _pageRepository = pageRepository;
+    }
+    
+    public async Task<PageDto> Handle(GetPageCommand request, CancellationToken cancellationToken)
+    {
+        GetPageCommandValidator validator = new(_pageRepository);
+        ValidationResultHandler.Handle(await validator.ValidateAsync(request, cancellationToken), request);
+
+        Page page = (await _pageRepository.GetByIdAsync(request.PageId))!;
+        return _mapper.Map<PageDto>(page);
+    }
+}
---
+++ Src/OmmelSamvirke.Application/Features/Pages/Pages/Validators/GetPageCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using OmmelSamvirke.Application.Features.Pages.Pages.Queries;
+using OmmelSamvirke.Domain.Features.Pages.Interfaces.Repositories;
+
+namespace OmmelSamvirke.Application.Features.Pages.Pages.Validators;
+
+public class GetPageCommandValidator : AbstractValidator<GetPageCommand>
+{
+    private readonly IPageRepository _pageRepository;
+
+    public GetPageCommandValidator(IPageRepository pageRepository)
+    {
+        _pageRepository = pageRepository;
+        
+        RuleFor(x => x.PageId)
+            .MustAsync(PageExists)
+            .WithMessage("Page does not exist");
+    }
+    
+    private async Task<bool> PageExists(int pageId, CancellationToken cancellationToken)
+    {
+        return await _pageRepository.GetByIdAsync(pageId) is not null;
+    }
+}
