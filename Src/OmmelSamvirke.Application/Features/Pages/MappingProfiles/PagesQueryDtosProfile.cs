a3049d58aca8152e99cf72d71c2e2963cf6629d9
Tobias Kristensen <tobiaskristensen12@gmail.com>
2023-07-16 17:30:54 +0200

    refactor: Remove ValueResolvers from Query profile
    The value resolvers are not used anymore, thus they have been removed.

--- Src/OmmelSamvirke.Application/Features/Pages/MappingProfiles/PagesQueryDtosProfile.cs
+++ Src/OmmelSamvirke.Application/Features/Pages/MappingProfiles/PagesQueryDtosProfile.cs
@@ -2,7 +2,6 @@
 using OmmelSamvirke.Application.Features.Pages.DTOs.Queries;
 using OmmelSamvirke.Application.Features.Pages.DTOs.Queries.ContentBlockData;
 using OmmelSamvirke.Application.Features.Pages.DTOs.Queries.Converters;
-using OmmelSamvirke.Application.Features.Pages.DTOs.Queries.ValueResolvers;
 using OmmelSamvirke.Domain.Features.Pages.Models;
 using OmmelSamvirke.Domain.Features.Pages.Models.ContentBlockData;
 using OmmelSamvirke.Domain.Features.Pages.Models.ContentBlocks;
@@ -13,12 +12,8 @@ public class PagesQueryDtosProfile : Profile
 {
     public PagesQueryDtosProfile()
     {
-        CreateMap<PageTemplate, PageTemplateQueryDto>()
-            .ForMember(dest => dest.ContentBlocks, 
-                opt => opt.MapFrom<ContentBlockToDtoResolver>())
-            .ReverseMap()
-            .ForMember(dest => dest.ContentBlocks, 
-                opt => opt.MapFrom<DtoToContentBlockResolver>());
+        CreateMap<PageTemplate, PageTemplateQueryDto>();
+        
         CreateMap<ContentBlock, ContentBlockQueryDto>().ConvertUsing<ContentBlockToContentBlockQueryDtoConverter>();
         CreateMap<ContentBlockQueryDto, ContentBlock>().ConvertUsing<ContentBlockQueryDtoToContentBlockConverter>();
         CreateMap<ContentBlockQueryDto, HeadlineBlock>().ReverseMap();
@@ -27,7 +22,6 @@ public class PagesQueryDtosProfile : Profile
         CreateMap<ContentBlockQueryDto, SlideshowBlock>().ReverseMap();
         CreateMap<ContentBlockQueryDto, TextBlock>().ReverseMap();
         CreateMap<ContentBlockQueryDto, VideoBlock>().ReverseMap();
-        CreateMap<ContentBlockLayoutConfiguration, ContentBlockLayoutConfigurationQueryDto>().ReverseMap();
         CreateMap<PageTemplateWithoutContentBlocksQueryDto, PageTemplate>().ReverseMap();
         CreateMap<HeadlineBlockData, HeadlineBlockDataQueryDto>();
         CreateMap<ImageBlockData, ImageBlockDataQueryDto>();
@@ -35,6 +29,8 @@ public class PagesQueryDtosProfile : Profile
         CreateMap<SlideshowBlockData, SlideshowBlockDataQueryDto>();
         CreateMap<TextBlockData, TextBlockDataQueryDto>();
         CreateMap<VideoBlockData, VideoBlockDataQueryDto>();
+        
         CreateMap<Page, PageQueryDto>().ReverseMap();
+        CreateMap<ContentBlockLayoutConfiguration, ContentBlockLayoutConfigurationQueryDto>().ReverseMap();
     }
 }
